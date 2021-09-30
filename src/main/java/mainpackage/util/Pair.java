1574b5b32793cde94a536267465d7038d29b9522
Marc Ziegler <marc.ziegler@smaint.de>
2021-09-30 16:43:39 +0200

    Fix in pair equals method for arrays #2

--- src/main/java/mainpackage/util/Pair.java
+++ src/main/java/mainpackage/util/Pair.java
@@ -8,7 +8,7 @@ public record Pair<A,B>(A one, B two) {
     public boolean equals(Object o)
     {
         if (o instanceof Pair<?, ?> pair) {
-            if (two.getClass().isArray()) { //special case for signatures
+            if (one.getClass().isArray() || two.getClass().isArray()) { //array equals
                 return one.equals(pair.one) && Objects.deepEquals(two, pair.two);
             }
             return one.equals(pair.one) && two.equals(pair.two);
