533e7a6f389a5ff4d9e96cf15d063aa2e25e3f5b
Marc Ziegler <marc.ziegler@smaint.de>
2021-09-30 16:41:30 +0200

    Fix in pair equals method for arrays

--- src/main/java/mainpackage/util/Pair.java
+++ src/main/java/mainpackage/util/Pair.java
@@ -1,14 +1,15 @@
 package mainpackage.util;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public record Pair<A,B>(A one, B two) {
     @Override
     public boolean equals(Object o)
     {
         if (o instanceof Pair<?, ?> pair) {
-            if (two instanceof byte[]) { //special case for signatures
-                return one.equals(pair.one) && Arrays.equals((byte[])two, (byte[])pair.two);
+            if (two.getClass().isArray()) { //special case for signatures
+                return one.equals(pair.one) && Objects.deepEquals(two, pair.two);
             }
             return one.equals(pair.one) && two.equals(pair.two);
         }
